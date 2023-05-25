8576cb1a36421667ad1ba6526be0b045a62c2996
KHashimoto3 <85172807+KHashimoto3@users.noreply.github.com>
2023-05-25 11:51:13 +0900

    containerをboxで囲んだ。

--- mock/src/components/MainPage.tsx
+++ mock/src/components/MainPage.tsx
@@ -1,4 +1,4 @@
-import { Grid } from "@mui/material";
+import { Box, Grid } from "@mui/material";
 import { Assist } from "./Assist";
 import { Form } from "./Form";
 
@@ -6,14 +6,16 @@ export const MainPage = () => {
     return (
         <div>
             <h1>MainPage</h1>
+            <Box>
             <Grid container spacing={2}>
-                <Grid item xs={5} style={{"background": "#fff7d4"}}>
+                <Grid item xs={5} style={{"background": "#fff7d4", height: "500px"}}>
                     <Assist />
                 </Grid>
-                <Grid item xs={7} style={{"background": "#d4f4ff"}}>
+                <Grid item xs={7} style={{"background": "#d4f4ff", height: "500px"}}>
                     <Form />
                 </Grid>
             </Grid>
+            </Box>
         </div>
     )
 };
