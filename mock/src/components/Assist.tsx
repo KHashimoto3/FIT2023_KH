4b6631aa0f21da678b7d2b5b949c4974ac7ef663
KHashimoto3 <85172807+KHashimoto3@users.noreply.github.com>
2023-05-25 12:13:24 +0900

    ヒントを文字列として3つ表示させた。

--- mock/src/components/Assist.tsx
+++ mock/src/components/Assist.tsx
@@ -2,9 +2,27 @@ import { Container } from "@mui/material";
 
 export const Assist = () => {
 
+    const hintList = [
+        {
+            "hint": "ヒント１"
+        },
+        {
+            "hint": "ヒント２"
+        },
+        {
+            "hint": "ヒント３"
+        }
+    ];
+
     return (
         <Container maxWidth="md">
             <h1>Assist</h1>
+            {hintList.map((hint, index) => {
+                return (
+                    <p>ヒント{index + 1}: {hint.hint}</p>
+                )
+            })
+            }
         </Container>
     )
 };
