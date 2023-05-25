ab7ed5ef142f3c709b43ce53f9f46cbc4afd61e5
KHashimoto3 <85172807+KHashimoto3@users.noreply.github.com>
2023-05-25 11:22:46 +0900

    assistとformを2つのエリアに分けた。

--- mock/src/components/Assist.tsx
+++ mock/src/components/Assist.tsx
@@ -1,6 +1,14 @@
 export const Assist = () => {
+
+    const assistAreaStyle: React.CSSProperties = {
+        width: '40%',
+        height: '100%',
+        backgroundColor: 'lightblue',
+        float: 'left'
+    };
+
     return (
-        <div>
+        <div style={assistAreaStyle}>
             <h1>Assist</h1>
         </div>
     )
--- mock/src/components/Form.tsx
+++ mock/src/components/Form.tsx
@@ -1,6 +1,14 @@
 export const Form = () => {
+
+    const formAreaStyle: React.CSSProperties = {
+        width: '60%',
+        height: '100%',
+        backgroundColor: 'lightgreen',
+        marginLeft: '40%'
+    };
+
     return (
-        <div>
+        <div style={formAreaStyle}>
             <h1>Form</h1>
         </div>
     )
