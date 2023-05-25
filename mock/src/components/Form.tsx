e653f500cad41d0bcf87438c87037b1223158d1e
KHashimoto3 <85172807+KHashimoto3@users.noreply.github.com>
2023-05-25 11:59:48 +0900

    formのデータを取ってこれるようにした。

--- mock/src/components/Form.tsx
+++ mock/src/components/Form.tsx
@@ -1,13 +1,13 @@
+import { useState } from "react";
 import { Container } from "@mui/material";
 
 export const Form = () => {
-
+    const [formValue, setFormValue] = useState<string>("");
     return (
         <Container maxWidth="md">
             <h1>Form</h1>
-            <input type="text" size={20} /><br />
-            <input type="text" size={20} /><br />
-            <input type="text" size={20} /><br />
+            <input type="text" size={20} onChange={(event) => setFormValue(event.target.value)} /><br />
+            <p>入力した値：{formValue}</p>
         </Container>
     )
 };
