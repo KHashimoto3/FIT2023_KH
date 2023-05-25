b10e78023701c736482837c463389f0641dbb5b6
KHashimoto3 <85172807+KHashimoto3@users.noreply.github.com>
2023-05-25 11:51:29 +0900

    formコンポーネントに試しにinputを3つ置いた。

--- mock/src/components/Form.tsx
+++ mock/src/components/Form.tsx
@@ -5,6 +5,9 @@ export const Form = () => {
     return (
         <Container maxWidth="md">
             <h1>Form</h1>
+            <input type="text" size={20} /><br />
+            <input type="text" size={20} /><br />
+            <input type="text" size={20} /><br />
         </Container>
     )
 };
