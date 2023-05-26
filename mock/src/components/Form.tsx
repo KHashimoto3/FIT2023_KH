5535bc92b8cfd3f311f2af94f8355d94402b66c0
KHashimoto3 <85172807+KHashimoto3@users.noreply.github.com>
2023-05-26 13:15:52 +0900

    不要なテストコードを削除した。

--- mock/src/components/Form.tsx
+++ mock/src/components/Form.tsx
@@ -17,9 +17,6 @@ export const Form = (props: Props) => {
             <Typography variant="h4">フォーム</Typography>
             <input type="text" size={20} onChange={(event) => setFormValue(event.target.value)} onFocus={() => changeFormDataIndex(0)} /><br />
             <input type="text" size={20} onChange={(event) => setFormValue2(event.target.value)} onFocus={() => changeFormDataIndex(1)} /><br />
-
-            <Typography variant="h5">入力値_上：{formValue}</Typography>
-            <Typography variant="h5">入力値_下：{formValue2}</Typography>
         </Container>
     )
 };
