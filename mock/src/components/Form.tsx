5eff82ca3af46d76e87bc3c5a9a8ee08b2857527
KHashimoto3 <85172807+KHashimoto3@users.noreply.github.com>
2023-06-07 15:53:46 +0900

    必要な入力欄を配置し、フォントサイズを調整した。

--- mock/src/components/Form.tsx
+++ mock/src/components/Form.tsx
@@ -12,17 +12,40 @@ export const Form = (props: Props) => {
     //MainPageから渡された、ヒントのリストのインデックスを変更する関数
     const changeFormDataIndex = props.changeFormDataIndex;
 
+    const commentStyle = {
+        color: "#39b359",
+        fontSize: "16pt"
+    }
+
+    const preStyle = {
+        fontSize: "16pt"
+    }
+
+    const inputStyle = {
+        fontSize: "16pt"
+    }
+
+    const textAreaStyle = {
+        fontSize: "16pt"
+    }
+
     return (
         <Container maxWidth="md">
             <Typography variant="h4">フォーム</Typography>
 
-            <pre>
-                <textarea cols={50} rows={2} onFocus={() => changeFormDataIndex(0)} /><br />
+            <pre style={preStyle}>
+                <pre style={commentStyle}>/**</pre>
+                <textarea style={commentStyle} cols={40} rows={2} /><br />
+                <pre style={commentStyle}>*/</pre>
+                <textarea style={textAreaStyle} cols={40} rows={2} onFocus={() => changeFormDataIndex(0)} /><br />
                 int main(void){'{\n'}
-                {'\t'}<textarea cols={50} rows={2} onFocus={() => changeFormDataIndex(1)} /><br />
+                {'\t'}<textarea style={textAreaStyle} cols={40} rows={2} onFocus={() => changeFormDataIndex(1)} /><br />
                 {'\t'}//入力値を受け取る{'\n'}
-                {'\t'}for {'('}<input type="text" size={5} onFocus={() => changeFormDataIndex(2)} />{'; '}<input type="text" size={5} onFocus={() => changeFormDataIndex(2)} />{'; '}<input type="text" size={5} onFocus={() => changeFormDataIndex(2)} />{') {\n'}
-                {'\t'}{'\t'}<textarea cols={50} rows={2} onFocus={() => changeFormDataIndex(3)} /><br />
+                {'\t'}for {'('}<input style={inputStyle} type="text" size={5} onFocus={() => changeFormDataIndex(2)} />{'; '}<input style={inputStyle} type="text" size={5} onFocus={() => changeFormDataIndex(2)} />{'; '}<input style={inputStyle} type="text" size={5} onFocus={() => changeFormDataIndex(2)} />{') {\n'}
+                {'\t'}{'\t'}<textarea style={textAreaStyle} cols={40} rows={2} onFocus={() => changeFormDataIndex(3)} /><br />
+                {'\t'}{'}'}<br />
+                {'\t'}return 0;<br />
+                {'}'}
             </pre>
         </Container>
     )
